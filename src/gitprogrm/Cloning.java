54ba71823589f570d17216e7e391ef9148ed1cae
velmurugan093 <rajmurugan093@gmail.com>
2019-09-28 17:53:38 +0530

    Added new class

---
+++ src/gitprogrm/Cloning.java
@@ -0,0 +1,15 @@
+package gitprogrm;
+
+public class Cloning {
+	public static void main(String[] args) {
+		
+	
+	int a=10;
+	int b=23;
+	int diff;
+	diff=a-b;
+	System.out.println("num is"+diff);
+	
+
+}
+}
